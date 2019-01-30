6fb8f7bfd6fea72621d1d3063b059af8f9e1aebd
ZPPD <zhana.p.d@gmail.com>
2019-01-30 14:32:39 -0500

    bar chart

--- dist/js/dashboard.js
+++ dist/js/dashboard.js
@@ -1,77 +1,87 @@
+/*===== Bar Chart ======*/
 // chart options
-// const options = {
-//   chart: {
-//     height: 300,
-//     width: "500px",
-//     type: "line",
-//     background: "#f4f4f4",
-//     foreColor: "#333"
-//   },
-//   series: [
-//     {
-//       name: "Sales",
-//       data: [
-//         65,
-//         35,
-//         52,
-//         53,
-//         37,
-//         24,
-//         21,
-//         57,
-//         55,
-//         93,
-//         63,
-//         48,
-//         39,
-//         54,
-//         35,
-//         19,
-//         47,
-//         41,
-//         39,
-//         58,
-//         68,
-//         49
-//       ]
-//     }
-//   ],
-//   xaxis: {
-//     categories: [
-//       "01 Jan",
-//       "02 Jan",
-//       "03 Jan",
-//       "04 Jan",
-//       "05 Jan",
-//       "06 Jan",
-//       "07 Jan",
-//       "08 Jan",
-//       "09 Jan",
-//       "10 Jan",
-//       "11 Jan",
-//       "12 Jan",
-//       "13 Jan",
-//       "14 Jan",
-//       "15 Jan",
-//       "16 Jan",
-//       "17 Jan",
-//       "18 Jan",
-//       "19 Jan",
-//       "20 Jan",
-//       "21 Jan",
-//       "22 Jan"
-//     ]
-//   },
-//   title: {
-//     text: "Sales for January 2019",
-//     align: "left"
-//   }
-// };
+const options = {
+  chart: {
+    height: 300,
+    // width: "500px",
+    type: "bar",
+    background: "#f4f4f4",
+    foreColor: "#333"
+  },
+  colors: ["#4caf50"],
+  series: [
+    {
+      name: "Customers",
+      data: [
+        65,
+        35,
+        52,
+        53,
+        37,
+        24,
+        21,
+        57,
+        55,
+        93,
+        63,
+        48,
+        39,
+        54,
+        35,
+        19,
+        47,
+        41,
+        39,
+        58,
+        68,
+        49
+      ]
+    }
+  ],
+  xaxis: {
+    categories: [
+      "01 Jan",
+      "02 Jan",
+      "03 Jan",
+      "04 Jan",
+      "05 Jan",
+      "06 Jan",
+      "07 Jan",
+      "08 Jan",
+      "09 Jan",
+      "10 Jan",
+      "11 Jan",
+      "12 Jan",
+      "13 Jan",
+      "14 Jan",
+      "15 Jan",
+      "16 Jan",
+      "17 Jan",
+      "18 Jan",
+      "19 Jan",
+      "20 Jan",
+      "21 Jan",
+      "22 Jan"
+    ]
+  },
+  title: {
+    text: "Customers for January 2019",
+    align: "left"
+  }
+};
+// init chart
+const chart = new ApexCharts(document.querySelector("#bar-chart"), options);
 
+// render chart
+chart.render();
+
+/* ======= Synced Charts  =======*/
 Apex = {
   chart: {
-    height: 160,
-    width: "500px"
+    height: 180,
+    // width: "500px",
+    background: "#f4f4f4",
+    foreColor: "#333"
   },
   dataLabels: {
     enabled: false
@@ -132,7 +142,11 @@ var optionsLine1 = {
         max: 60
       })
     }
-  ]
+  ],
+  title: {
+    text: "Sales",
+    align: "left"
+  }
 };
 
 var chartLine1 = new ApexCharts(
@@ -156,7 +170,11 @@ var optionsline2 = {
         max: 30
       })
     }
-  ]
+  ],
+  title: {
+    text: "Expences",
+    align: "left"
+  }
 };
 
 var chartline2 = new ApexCharts(
@@ -180,7 +198,11 @@ var optionsArea = {
         max: 60
       })
     }
-  ]
+  ],
+  title: {
+    text: "Profits",
+    align: "left"
+  }
 };
 
 var chartArea = new ApexCharts(
@@ -213,11 +235,3 @@ function generateDayWiseTimeSeries(baseval, count, yrange) {
   }
   return series;
 }
-
-// init chart
-const chart = new ApexCharts(document.querySelector("#chart"), options);
-
-// render chart
-chart.render();
-
-// 22

f0e0f463546ed46bddfd00727f52bb258b648c8a
ZPPD <zhana.p.d@gmail.com>
2019-02-05 14:28:45 -0500

    responsive menu in all pages

--- dist/js/menu.js
+++ dist/js/menu.js
@@ -2,6 +2,8 @@
 const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
 const navItems = document.querySelectorAll(".menu-item");
+const menuNav = document.querySelector(".menu-nav");
+const loginBtn = document.querySelector(".login-btn");
 
 // set initial state of menu
 let showMenu = false;
@@ -11,12 +13,16 @@ function toggleMenu() {
   if (!showMenu) {
     menuBtn.classList.add("close");
     menu.classList.add("show");
+    loginBtn.classList.add("show");
+    menuNav.classList.add("show");
     navItems.forEach(item => item.classList.add("show"));
     // set menu state
     showMenu = true;
   } else {
     menuBtn.classList.remove("close");
     menu.classList.remove("show");
+    loginBtn.classList.remove("show");
+    menuNav.classList.remove("show");
     navItems.forEach(item => item.classList.remove("show"));
     // set menu state
     showMenu = false;
